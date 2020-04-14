bd71cb3b8ad63a6cc25064ef389423d7ecda663d
Peter <ph31pe@gmail.com>
2020-04-14 22:03:52 +0100

    Huh

---
+++ Divinator.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template, request, redirect, url_for
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+
+@app.route("/duel")
+def duel():
+    return render_template("home.html")
+
+
+@app.route("/thousand_duels")
+def thousand_duels():
+    return render_template("home.html")
+
+
+@app.route("/goblins")
+def goblins():
+    return render_template("home.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
